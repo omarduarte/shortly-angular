908c4d82a8f73aea6aa34488168f658a6e55bb1f
omarduarte <omarduarte@gmail.com>
2015-01-16 20:49:34 -0800

    Added validation for shorten and signing in

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -4,9 +4,14 @@ angular.module('shortly.shorten', ['shortly.services'])
   // Your code here
   $scope.link = {};
   $scope.addLink = function (data) {
-    Links.post(data).then(function(res) {
-      // whaterver
-    });
+    var validateUrl = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
+    if (validateUrl.test(data)) {
+      Links.post({ url:data }).then(function(res) {
+        $scope.link.url = '';
+      });
+    } else {
+      window.alert('Invalid URL');
+    }
   };
 
 });
