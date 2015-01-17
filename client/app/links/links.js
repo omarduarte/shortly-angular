b5b97f6f8338db453ee2154c812974239b8b73bb
omarduarte <omarduarte@gmail.com>
2015-01-16 16:11:29 -0800

    LinksController working

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,19 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  $scope.data = {};
+  $scope.getLinks = function() {
+     Links.then(function(data) {
+      $scope.data.links = data;
+    });
+  };
+
+  $scope.getLinks();
+
+})
+.factory('Links', function($http) {
+  return $http.get("/api/links")
+  .then(function(resp) {
+    return resp.data;
+  });
 });
